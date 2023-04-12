2b86392de63e693d75cedf96a86f2f462c5746ba
peter-hazell <18005888+peter-hazell@users.noreply.github.com>
2023-04-12 16:27:12 +0100

    ECL-321: Bind auth action

--- app/uk/gov/hmrc/economiccrimelevyaccount/config/Module.scala
+++ app/uk/gov/hmrc/economiccrimelevyaccount/config/Module.scala
@@ -17,9 +17,11 @@
 package uk.gov.hmrc.economiccrimelevyaccount.config
 
 import com.google.inject.AbstractModule
+import uk.gov.hmrc.economiccrimelevyreturns.controllers.actions.{AuthorisedAction, BaseAuthorisedAction}
 
 class Module extends AbstractModule {
 
   override def configure(): Unit =
-    bind(classOf[AppConfig]).asEagerSingleton()
+    bind(classOf[AuthorisedAction]).to(classOf[BaseAuthorisedAction]).asEagerSingleton()
+    
 }
