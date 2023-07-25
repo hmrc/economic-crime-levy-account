67b0b45a7774d720622dbd364171d4af166cde96
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2023-07-25 09:02:08 +0100

    IT fix

--- test/uk/gov/hmrc/economiccrimelevyaccount/connectors/IntegrationFrameworkConnectorSpec.scala
+++ test/uk/gov/hmrc/economiccrimelevyaccount/connectors/IntegrationFrameworkConnectorSpec.scala
@@ -40,7 +40,7 @@ class IntegrationFrameworkConnectorSpec extends SpecBase {
         eitherResult: Either[FinancialDataErrorResponse, FinancialDataResponse]
       ) =>
         val expectedUrl =
-          s"${appConfig.integrationFrameworkUrl}/penalty/financial-data/zecl/$eclRegistrationReference/ECL"
+          s"${appConfig.integrationFrameworkUrl}/penalty/financial-data/ZECL/$eclRegistrationReference/ECL"
 
         val expectedHeaders: Seq[(String, String)] = Seq(
           (HeaderNames.AUTHORIZATION, s"Bearer ${appConfig.integrationFrameworkBearerToken}"),
