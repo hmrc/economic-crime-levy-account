b8fb511ecbf71317958e2213b966a31c55724a09
Daniel P. Doyle <3020488+TheDanielDoyle@users.noreply.github.com>
2023-08-07 17:21:00 +0100

    ECL-445: Removed payment documenttype as not ECL related

--- app/uk/gov/hmrc/economiccrimelevyaccount/connectors/IntegrationFrameworkConnector.scala
+++ app/uk/gov/hmrc/economiccrimelevyaccount/connectors/IntegrationFrameworkConnector.scala
@@ -52,7 +52,7 @@ class IntegrationFrameworkConnector @Inject() (
     (QueryParams.ACCRUING_INTEREST, "true"),
     (QueryParams.CLEARED_ITEMS, "true"),
     (QueryParams.LOCK_INFORMATION, "true"),
-    (QueryParams.PAYMENT, "true"),
+    (QueryParams.PAYMENT, "false"),
     (QueryParams.PENALTY_DETAILS, "true"),
     (QueryParams.POSTED_INTEREST, "true"),
     (QueryParams.REGIME_TOTALISATION, "true"),
