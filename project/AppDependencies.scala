52f859db559985909d85a2d2896238489b1f61ec
Liandra Giacomine <liandra11@hotmail.com>
2023-10-11 12:17:06 +0100

    ECL-598: Updated dependency versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "7.19.0"
+  private val hmrcBootstrapVersion = "7.22.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % hmrcBootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % hmrcBootstrapVersion
   )
 
   val test: Seq[ModuleID]    = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-28"   % hmrcBootstrapVersion,
-    "org.mockito"          %% "mockito-scala"            % "1.17.12",
+    "org.mockito"          %% "mockito-scala"            % "1.17.27",
     "org.scalatestplus"    %% "scalatestplus-scalacheck" % "3.1.0.0-RC2",
     "com.danielasfregola"  %% "random-data-generator"    % "2.9",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"    % "1.1.0"
