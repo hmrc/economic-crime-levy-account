4bf9269b072f95b8747f1965edb272e777ae0c46
Athish Thayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-02-13 11:26:06 +0000

    DDCYLS-5995 - dependency upgrades (#44)
    * dependency upgrades
    
    * plugin updates
    
    * removed JSON.logger.xml

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,19 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "8.4.0"
+  private val hmrcBootstrapVersion = "9.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"  %% "bootstrap-backend-play-30" % hmrcBootstrapVersion,
-    "io.lemonlabs" %% "scala-uri"                 % "4.0.3",
-
+    "io.lemonlabs" %% "scala-uri"                 % "4.0.3"
   )
 
-  val test: Seq[ModuleID]    = Seq(
+  val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30" % hmrcBootstrapVersion,
     "org.mockito"          %% "mockito-scala"          % "1.17.30",
     "org.scalatestplus"    %% "scalacheck-1-17"        % "3.2.18.0",
-    "com.danielasfregola"  %% "random-data-generator"  % "2.9",
+    "com.danielasfregola"   %% "random-data-generator"  % "2.9",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"  % "1.1.0"
   ).map(_ % "test, it")
 
